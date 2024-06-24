7e27121108c568e3b0b269dbf27f30b3060c6709
Silvina Figueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2024-06-23 17:45:02 -0700

    fetch all candidates

--- routes/candidates.mjs
+++ routes/candidates.mjs
@@ -4,7 +4,24 @@ import database from '../db/conn.mjs'
 const router = express.Router()
 
 
-router.post('/', async (req, res) => {
+router.get('/all', async (req, res) => {
+    try {
+        const db = await database
+        let candidateCollection = db.collection("Candidate")
+
+        // Fetch all the candidates in an array 
+        const allCandidates = await candidateCollection.find({}).toArray()
+
+        res.status(200).json(allCandidates)
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).send({ message: `Error fetching documents ${err.message}` })
+    }
+})
+
+
+router.post('/add', async (req, res) => {
     const { name, email, location } = req.body
     console.log(name, email, location)
 
@@ -24,7 +41,7 @@ router.post('/', async (req, res) => {
         }
 
         res.status(201).json(newCandidate)
-        
+
     } catch (err) {
         if (err.name === 'MongoError' && err.code === 121) {
             res.status(400).send({ message: "Validation error: " + err.message })
